e493f6c27c96f27e5c70cc394ffae2f317cf3632
Joel Graycar <jgraycar@berkeley.edu>
2014-06-28 17:25:24 -0700

    Started working on performance functionality

--- app/controllers/past_performances_controller.rb
+++ app/controllers/past_performances_controller.rb
@@ -1,2 +1,53 @@
 class PastPerformancesController < ApplicationController
+  before_action :authenticate_user!, :except => [:show, :index]
+
+  def new
+    @past_performance = PastPerformance.new
+  end
+
+  def create
+    @past_performance = PastPerformance.new(past_performance_params)
+ 
+    if @past_performance.save
+      redirect_to @past_performance
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def past_performance_params
+    params.require(:past_performance).permit(:date, :location,
+                                             :description, :title)
+  end
+
+  public
+  def index
+    @past_performances = PastPerformance.all
+  end
+
+  def show
+    @past_performance = PastPerformance.find(params[:id])
+  end
+
+  def edit
+    @past_performance = PastPerformance.find(params[:id])
+  end
+
+  def update
+    @past_performance = PastPerformance.find(params[:id])
+
+    if @past_performance.update(past_performance_params)
+      redirect_to @past_performance
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @past_performance = PastPerformance.find(params[:id])
+    @past_performance.destroy
+    
+    redirect_to past_performances_path
+  end
 end
--- app/models/upcoming_performance.rb
+++ app/models/upcoming_performance.rb
@@ -1,2 +1,6 @@
 class UpcomingPerformance < ActiveRecord::Base
+  validates :date, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
 end
