fb824b134ef0e948f3d08223406aeb62f15aadd9
Joel Graycar <jgraycar@berkeley.edu>
2014-07-01 21:07:36 -0700

    LOOOTS of styling done! Lookin not baaaad~~

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,12 +4,11 @@ class Member < ActiveRecord::Base
   validates :major, presence: true, length: {minimum: 2}
   validates :bio, presence: true
 
-  has_attached_file :avatar, :styles => { medium: "300x300#", thumb: "100x100#" },
+  has_attached_file :avatar, :styles => { thumb: "200x200#" },
   path: ":rails_root/public/system/members/:attachment/:id/:style/:filename",
   url: "/system/members/:attachment/:id/:style/:filename",
   default_url: "/images/:style/missing.png"
   include DeletableAttachment
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-
 
 end
