899276b12c3203259ebb6f03ecf92301bc2690e4
Joel Graycar <jgraycar@berkeley.edu>
2014-07-10 02:03:59 -0700

    Setting up mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,21 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Notify if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  # Set up mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    domain: ENV["GMAIL_DOMAIN"],
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+  }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -31,8 +44,6 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
