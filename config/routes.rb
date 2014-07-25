867fa4ac5beeee3ef6ec72e223ec61291a1f6235
Joel Graycar <jgraycar@berkeley.edu>
2014-07-24 17:09:32 -0700

    made facebook reload on homepage after logout

--- config/routes.rb
+++ config/routes.rb
@@ -28,14 +28,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'front_page#main'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
