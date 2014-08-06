0432b18f238384caff4ec2472e7b84315c5d6bbc
Joel Graycar <jgraycar@berkeley.edu>
2014-08-05 18:04:52 -0700

    refined wording of articles test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -13,6 +13,7 @@ describe Article do
   it "is invalid without text" do
     expect(build(:article, text: nil)).to_not be_valid
   end
+
   describe "getting all current articles" do
     it "returns all articles marked current" do
       current1 = create(:article, current: true)
@@ -20,6 +21,34 @@ describe Article do
       past = create(:article, current: false)
       expect(Article.current).to include(current1, current2)
       expect(Article.current).to_not include past
+    end
+  end
+
+  describe "generating a slug" do
+    before :each do
+      @go_us = create(:article, title: "cal raijin taiko the best")
+      @go_us_again = create(:article, title: "Cal Raijin Taiko the Best",
+                            date: Time.new(2014))
+      @keep_going_us = create(:article, title: "Cal raIJIN taIKO THE best",
+                              date: Time.new(2014, 3, 25))
+    end
+
+    context "no other article by same name exists" do
+      it "uses title as slug" do
+        expect(@go_us.slug).to eq("cal-raijin-taiko-the-best")
+      end
+    end
+
+    context "article by same name already exists" do
+      it "uses title-year as slug" do
+        expect(@go_us_again.slug).to eq("cal-raijin-taiko-the-best-2014")
+      end
+    end
+
+    context "article by same name and same year already exists" do
+      it "uses title-year-month-day as slug" do
+        expect(@keep_going_us.slug).to eq("cal-raijin-taiko-the-best-2014-03-25")
+      end
     end
   end
 end
