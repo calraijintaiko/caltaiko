778438d7b0cbdfacb897aed372da8f82ad3bce22
Joel Graycar <jgraycar@berkeley.edu>
2014-08-08 20:02:30 -0700

    finished performance controller spec

--- spec/controllers/performances_controller_spec.rb
+++ spec/controllers/performances_controller_spec.rb
@@ -26,5 +26,94 @@ describe PerformancesController do
       expect(assigns(:by_year)[@years[0]]).to match_array @created_past_year0
       expect(assigns(:by_year)[@years[1]]).to match_array @created_past_year1
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template "index"
+    end
+  end
+
+  describe "getting a specific performance" do
+    before :each do
+      @id = rand(1..100)
+      @itf = create(:performance, title: "international taiko festival",
+                    date: Time.new(2010), id: @id)
+      signed_in_as_a_valid_user
+    end
+
+    context "GET show" do
+      it "gets performance using slug" do
+        get :show, id: "international-taiko-festival-2010"
+        expect(assigns(:performance)).to eq @itf
+      end
+
+      it "gets performance using id" do
+        get :show, id: @id
+        expect(assigns(:performance)).to eq @itf
+      end
+
+      it "renders the show template" do
+        get :show, id: @id
+        expect(assigns(:performance)).to eq @itf
+      end
+    end
+
+    context "GET edit" do
+      it "gets performance using slug" do
+        get :edit, id: "international-taiko-festival-2010"
+        expect(assigns(:performance)).to eq @itf
+      end
+
+      it "gets performance using id" do
+        get :edit, id: @id
+        expect(assigns(:performance)).to eq @itf
+      end
+
+      it "renders the show template" do
+        get :edit, id: @id
+        expect(assigns(:performance)).to eq @itf
+      end
+    end
+  end
+
+  describe "getting all upcoming or past performances" do
+    before :each do
+      @years = ["2005", "2008"]
+      @created_upcoming = create_list(:performance, 4, date: Time.new(Time.now.year + 1))
+      @created_past_year0 = create_list(:performance, 5, date: Time.new(@years[0]))
+      @created_past_year1 = create_list(:performance, 7, date: Time.new(@years[1]))
+      @created_past = @created_past_year0 + @created_past_year1
+    end
+
+    context "GET upcoming" do
+      it "assigns @upcoming to all upcoming performances" do
+        get :upcoming
+        expect(assigns(:upcoming)).to match_array @created_upcoming
+      end
+
+      it "renders the upcoming template" do
+        get :upcoming
+        expect(response).to render_template "upcoming"
+      end
+    end
+
+    context "GET past" do
+      it "assigns @past to all past performances" do
+        get :past
+        expect(assigns(:past)).to match_array @created_past
+      end
+
+      it "assigns @by_year to all past performances broken down by year" do
+        get :index
+        expect(assigns(:by_year).keys).to match_array @years
+        expect(assigns(:by_year)[@years[0]]).to match_array @created_past_year0
+        expect(assigns(:by_year)[@years[1]]).to match_array @created_past_year1
+      end
+
+      it "renders the past template" do
+        get :past
+        expect(response).to render_template "past"
+      end
+    end
   end
 end
