1c6d706c5a9e348e441aa8b60fa40331e9107a8a
Joel Graycar <jgraycar@berkeley.edu>
2014-09-02 18:10:26 -0700

    added image galleries to media page, in thumbnail format & broken up by year

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,5 +16,10 @@ class PagesController < ApplicationController
       @videos_by_year[video.year.to_s] << video
     end
     @performances = Performance.have_images
+    @performances_by_year = Hash.new
+    @performances.each do |performance|
+      @performances_by_year[performance.date.strftime("%Y")] ||= []
+      @performances_by_year[performance.date.strftime("%Y")] << performance
+    end
   end
 end
