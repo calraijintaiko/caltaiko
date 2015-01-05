3ec10cacffaaab9e2718b3bbe8922333341cf53e
Joel Graycar <j.cx.graycar@gmail.com>
2015-01-05 02:21:51 -0800

    Fixed pages_controller to use new images? method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,7 @@ class PagesController < ApplicationController
   def media
     @videos = Video.all.order('year DESC')
     @videos_by_year = Video.by_year(@videos)
-    @performances = Performance.have_images.order('date DESC')
+    @performances = Performance.images?.order('date DESC')
     @performances_by_year = Performance.by_year(@performances)
   end
 end
