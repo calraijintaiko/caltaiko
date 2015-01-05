91f12ff64b3a6202a41c65aee87b217289bdd9f9
Joel Graycar <j.cx.graycar@gmail.com>
2015-01-05 03:28:04 -0800

    Beautified routes
    Added path names for most routes, so that views could reference names
    rather than direct URLs. Deleted unnecessary route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   root 'pages#front'
 
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
-  get 'performances/upcoming' => 'performances#upcoming'
-  get 'performances/past' => 'performances#past'
-  get 'performances/show/:id' => 'performances#show'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'news', to: 'articles#index', as: :news
+  get 'media', to: 'pages#media', as: :media
+
+  get 'performances/upcoming', to: 'performances#upcoming',
+                               as: :upcoming_performances
+  get 'performances/past', to: 'performances#past', as: :past_performances
+
   get 'members' => 'members#index'
-  get 'members/current' => 'members#current'
-  get 'members/alumni' => 'members#alumni'
-  get 'members/gen' => 'members#all_gens'
-  get 'members/gen/:id' => 'members#gen'
-  get 'members/database' => 'members#database'
-  get 'news' => 'articles#index'
-  get 'media' => 'pages#media'
+  get 'members/current', to: 'members#current', as: :current_members
+  get 'members/alumni', to: 'members#alumni', as: :alumni_members
+  get 'members/gen', to: 'members#all_gens', as: :members_all_gens
+  get 'members/gen/:id', to: 'members#gen', as: :members_gen
+  get 'members/database', to: 'members#database', as: :members_database
 
   resources :members
   resources :videos
@@ -25,18 +28,17 @@ Rails.application.routes.draw do
   resources :articles
 
   if Rails.env.production?
-    devise_for :users, controllers: { registrations: "registrations" }
+    devise_for :users, controllers: { registrations: 'registrations' }
   else
     devise_for :users
   end
 
   devise_scope :user do
-    get "login", :to => "devise/sessions#new"
-    get "settings", :to => "devise/registrations#edit"
-    get "logout",   :to => "devise/sessions#destroy"
-    get "register", to: "devise/registrations#new"
+    get 'login', to: 'devise/sessions#new'
+    get 'settings', to: 'devise/registrations#edit'
+    get 'logout',   to: 'devise/sessions#destroy', as: :logout
+    get 'register', to: 'devise/registrations#new'
   end
-
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
@@ -80,5 +82,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
 end
