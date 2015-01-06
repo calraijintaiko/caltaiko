5ed6b25b1e2532f846426c8e8091b05ee62013cb
Joel Graycar <j.cx.graycar@gmail.com>
2015-01-05 16:39:05 -0800

    Removed unneeded members mailer

--- app/mailers/member_mailer.rb
+++
@@ -1,9 +0,0 @@
-# Members Mailer
-class MemberMailer < ActionMailer::Base
-  default from: 'caltaiko@gmail.com'
-
-  def email_member(member)
-    @member = member
-    mail(to: @member.email, subject: 'Notice - Cal Raijin Taiko')
-  end
-end
