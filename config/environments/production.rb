9996f9826fc5e46b66ef55822179fd80accdb041
Joel Graycar <jgraycar@berkeley.edu>
2015-01-22 00:03:34 -0800

    Updating deprecated serve_static_assets method in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in
+  # config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -16,11 +17,12 @@ Rails.application.configure do
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # For large-scale production use, consider using a caching
+  # reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -38,7 +40,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
@@ -54,28 +57,32 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV["CDN_DISTRIBUTION_URL"]
+  config.action_controller.asset_host = ENV['CDN_DISTRIBUTION_URL']
 
   # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  # application.js, application.css, and all non-JS/CSS in app/assets
+  # folder are already added.
   # config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # Set this to true and configure the email server for immediate
+  # delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
   # Set up mailer
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: 'smtp.gmail.com',
     port: 587,
-    authentication: "plain",
+    authentication: 'plain',
     enable_starttls_auto: true,
-    domain: ENV["GMAIL_DOMAIN"],
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    domain: ENV['GMAIL_DOMAIN'],
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
   }
-  config.action_mailer.default_url_options = { :host => 'http://caltaiko.herokuapp.com' }
+  config.action_mailer.default_url_options = {
+    host: 'http://caltaiko.herokuapp.com'
+  }
   config.action_mailer.asset_host = 'http://caltaiko.herokuapp.com'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
@@ -104,5 +111,4 @@ Rails.application.configure do
       s3_host_name: ENV['S3_HOST_NAME']
     }
   }
-
 end
