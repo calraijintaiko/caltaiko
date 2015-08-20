ce7d4f2ef3351841b8293cd8675748dc8306155b
Joel Graycar <jgraycar@berkeley.edu>
2015-08-19 22:41:41 -0700

    Closes #12 - Changed front page carousel from orbit to Slick

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( :modernizr )
+
+# Slick Carousel assets
+Rails.application.config.assets.precompile += %w( slick.css )
+Rails.application.config.assets.precompile += %w( slick-theme.css )
+Rails.application.config.assets.precompile += %w( ajax-loader.gif )
