f40366fef59eda3fb51e4f7c82c2dee86da6f37a
Joel Graycar <jgraycar@berkeley.edu>
2015-10-28 21:41:20 -0700

    Send Devise password-reset emails from caltaiko.website

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'caltaiko@gmail.com'
+  config.mailer_sender = 'caltaiko.website@gmail.com'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
