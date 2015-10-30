8f0591fc3ca6b117257a59e42a1c4df18b8a2c98
Joel Graycar <jgraycar@berkeley.edu>
2015-10-29 18:09:10 -0700

    Fix specs broken by moving contact view to messages

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
-
+  describe 'GET new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template 'new'
+    end
+  end
 end
